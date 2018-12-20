db2f48cd7e9b10f13fce243d01cdf33249feb4ba
Duncan Forgan <dhf3@st-andrews.ac.uk>
2018-12-20 10:58:30 +0000

    (science example) Update example script with uniform distribution of tbegin for transmitters

--- examples/science_example.py
+++ examples/science_example.py
@@ -70,10 +70,10 @@ for irun in range(nruns):
     popn.generate_identical_transmitters(N_transmitters=N_transmitters, strategy=strat_obs,semimajoraxis =None, inclination=None, mean_anomaly=None, longascend=None, nu=freq, bandwidth=band, solidangle=solidangle, power=power, spatial_distribution="GHZ",tbegin=popn.tbegin, tend=popn.tend)
 
     # Randomly assign broadcast parameters
-    popn.assign_uniform_broadcast_parameters(nu_parameters=[1.40e9,1.5e9], bandwidth_parameters=[1.0e0,1.0e1], pulseduration_parameters=[0.1,5.0], pulseinterval_parameters=[0.1,5.0], power_parameters=[1.0e17,1.0e18])
+    popn.assign_uniform_broadcast_parameters(nu_parameters=[1.40e9,1.5e9], bandwidth_parameters=[1.0e0,1.0e1], pulseduration_parameters=[0.1,5.0], pulseinterval_parameters=[0.1,5.0], power_parameters=[1.0e17,1.0e18],tbegin_parameters=[-100000.0,-50000.0])
 
     # Run simulation
-    popn.run_simulation(write_detections=True, make_plots=False, allskymap=True,delay_time=False)
+    popn.run_simulation(write_detections=True, make_plots=False, allskymap=True,delay_time=True)
 
 
     print ("Number of Detections: ",popn.ndetect)
